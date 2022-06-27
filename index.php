878df11d17707704d7368b76d75b9f422ff43b40
= <=>
2022-06-27 07:59:11 +0300

    poppup

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+
+
+<div class="button">
+    <a href="#" id="open_pop_up">Оставить заявку</a>
+</div>
+
+<div class="pop_up" id="pop_up">
+    <div class="pop_up_container">
+        <div class="pop_up_body" id="pop_up_body">
+            <p >Оставить заказ</p>
+            <form action="">
+                <input type="text" placeholder="Имя">
+                <input type="text" placeholder="Телефон">
+                <button>Оставить заказ</button>
+               
+            </form>
+            <div class="pop_up_close" id="pop_up_close">&#10006</div>
+        </div>
+    </div>
+</div>
+
+
+<script src="main.js"></script>
+</body>
+</html>
